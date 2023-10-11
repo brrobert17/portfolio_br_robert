c607b4da5af4c97333de3a44d93d2639faceeae7
brrobert17 <br.robert17@gmail.com>
2023-10-11 12:59:29 +0200

    first test

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+import styles from "@/app/page.module.css";
+
+export default function Page() {
+    return (
+        <main className={styles.main}>
+            <h1>Hello About World!</h1>
+        </main>
+    )
+}
---
+++ app/api/data/route.ts
@@ -0,0 +1,25 @@
+// pages/api/fetchData.ts
+
+import { NextApiRequest, NextApiResponse } from 'next';
+import 'firebase/firestore';
+import {doc, getDoc} from 'firebase/firestore';
+import {NextResponse} from "next/server";
+import {db} from "@/app/config/firebaseConfig";
+
+
+
+export async function GET(request: Request) {
+    try {
+        const ref = await doc(db, 'test', 'bNapj36Jkr9N8lPSRjNf');
+
+        const documentSnapshot = await getDoc(ref);
+        const data = documentSnapshot.data();
+
+        return NextResponse.json({data}, {status: 200})
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return NextResponse.json({error: 'Error fetching data'}, {status: 500})
+
+    }
+}
+
