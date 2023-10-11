7bdcb8ee03ffdb315e4e5ff56147c0c45e41b65f
brrobert17 <br.robert17@gmail.com>
2023-10-11 14:35:17 +0200

    6th test(conent-type)

--- app/components/BasicFetch/index.tsx
+++ app/components/BasicFetch/index.tsx
@@ -1,5 +1,11 @@
 export default async () => {
-    const response = await fetch(`${process.env.BASE_URL}/api/data`,{cache: "no-cache"})
+    const response = await fetch(`${process.env.BASE_URL}/api/data`,
+        {cache: "no-cache",
+        method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            }})
     const result =  await response.json();
     return(<h1>Hello {result.name}!</h1>);
 }
