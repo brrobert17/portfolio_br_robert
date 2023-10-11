7c7c9b1103eeaa0f1c7dafb4592536b127562d9c
brrobert17 <br.robert17@gmail.com>
2023-10-11 15:24:47 +0200

    7th test

--- app/api/data/route.ts
+++ app/api/data/route.ts
@@ -12,7 +12,7 @@ export async function GET(request: Request) {
         const documentSnapshot = await getDoc(ref);
         const data = documentSnapshot.data();
 
-        return NextResponse.json(data, {status: 200})
+        return NextResponse.json({data})
     } catch (error) {
         console.error('Error fetching data:', error);
         return NextResponse.json({error: 'Error fetching data'}, {status: 500})
--- app/components/BasicFetch/index.tsx
+++ app/components/BasicFetch/index.tsx
@@ -1,10 +1,17 @@
 export default async () => {
-    const response = await fetch(`${process.env.BASE_URL}/api/data`,
-        {cache: "no-cache",
-        method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-            }})
-    const result =  await response.json();
-    return(<h1>Hello {result.name}!</h1>);
+    let dd = "bello"
+    try {
+        const response = await fetch(`${process.env.BASE_URL}/api/data`,
+            {cache: "no-cache",
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                }})
+        const result =  await response.json();
+        dd = result.data.name;
+    } catch (e) {
+        console.log("error fetch front"+e)
+    }
+
+    return(<h1>Hello {dd}!</h1>);
 }
